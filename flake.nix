04912b903767d7f7d0559b085e6297c1d4e6f276
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-05 22:34:22 -0600

    flake

--- flake.nix
+++ flake.nix
@@ -1,34 +1,34 @@
 {
-  description = "My development environment with Jupyter";
+  description = "Jupyter notebook dev shell with matplotlib, numpy, scipy, pandas";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      devShells = {
-        inherit system;
-        default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.chord
-            (pkgs.python3.withPackages (ps: with ps; with pkgs.python3Packages; [
-              jupyter
-              notebook
-              ipython
-              matplotlib
-              numpy
-              scipy
-              pandas
-            ]))
-          ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-          shellHook = "jupyter notebook";
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          jupyter
+          notebook
+          matplotlib
+          numpy
+          scipy
+          pandas
+          ipykernel
+        ]);
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ pythonEnv ];
+
+          shellHook = ''
+            echo "🐍 Jupyter environment ready!"
+            echo "Run: jupyter notebook"
+          '';
         };
-      };
-    };
+      });
 }
