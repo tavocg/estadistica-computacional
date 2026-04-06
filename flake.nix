9006ee6c436f06dd739029f147dbab5a88a6de97
tavocg <gustavo.calvo.cr@gmail.com>
2026-04-05 22:31:45 -0600

    update

--- flake.nix
+++ flake.nix
@@ -1,28 +1,34 @@
 {
+  description = "My development environment with Jupyter";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
     in
     {
-      devShells.default = { pkgs, ... }: pkgs.mkShell {
-        buildInputs = [
-          pkgs.chord
-          (pkgs.python3.withPackages (ps: with ps; with pkgs.python3Packages; [
-            jupyter
-            notebook
-            ipython
-            pandas
-            numpy
-            scipy
-            matplotlib
-          ]))
-        ];
+      devShells = {
+        inherit system;
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.chord
+            (pkgs.python3.withPackages (ps: with ps; with pkgs.python3Packages; [
+              jupyter
+              notebook
+              ipython
+              matplotlib
+              numpy
+              scipy
+              pandas
+            ]))
+          ];
 
-        shellHook = "jupyter notebook";
+          shellHook = "jupyter notebook";
+        };
       };
     };
 }
